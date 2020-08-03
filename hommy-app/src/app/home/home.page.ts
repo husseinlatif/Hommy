a2b64c01909de53b17f21e82591fb2c1671be91b
husseinlatif <hussein.latif@poli.ufrj.br>
2020-08-02 21:42:02 -0300

    integracao

--- hommy-app/src/app/home/home.page.ts
+++ hommy-app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service'
 
 @Component({
   selector: 'app-home',
@@ -7,18 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  public allRepublics = [];
+
   republics = [{},{},{},{},{}];
 
-  constructor() { }
+  constructor(public searchService: SearchService) { }
 
   ngOnInit() {
-    for (let i=0; i<5; i++){
-      this.republics[i].name= 'Republic ' + i;
-      this.republics[i].price= Math.floor(Math.random()*1500);
-      this.republics[i].numOfAvailableRooms= Math.floor(Math.random()*5);
-      this.republics[i].numOfBathrooms= Math.floor(Math.random()*5);
-      }
-
+   for (let i=0; i<5; i++){
+     this.republics[i].name= 'Republic ' + i;
+     this.republics[i].price= Math.floor(Math.random()*1500);
+     this.republics[i].numOfAvailableRooms= Math.floor(Math.random()*5);
+     this.republics[i].numOfBathrooms= Math.floor(Math.random()*5)}
+  
+    this.listAllRepublics();
   }
 
-}
+  listAllRepublics() {
+    this.searchService.getListRepublic().subscribe((res) => {
+      this.allRepublics = res[0];
+      console.log(this.allRepublics);
+    });
+  }
+
+  }
